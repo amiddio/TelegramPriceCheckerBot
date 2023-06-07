f259f9cf662dbe029b4960d966b083cf3ce518b6
d.shkinin <amiddio.dev@gmail.com>
2023-06-07 18:40:34 +0400

    Added parsing Blackdiamondequipment and fixed issues

---
+++ parser/blackdiamondequipment.py
@@ -0,0 +1,37 @@
+from bs4 import BeautifulSoup
+from parser.parser_base import ParserBase
+
+
+class Blackdiamondequipment(ParserBase):
+
+    def parse(self, url: str) -> str | None:
+        """
+        Метод парсинга класса
+
+        :param url: str
+        :return: str | None
+        """
+
+        soup: BeautifulSoup = self.get_html_parser(url)
+        if not soup:
+            return
+
+        res = soup.find('span', class_='low-regular-price')
+        if res:
+            return res.text.strip()
+
+    def parse_title(self, url: str) -> str | None:
+        """
+        Парсим название продукта
+
+        :param url: str
+        :return: str | None
+        """
+
+        soup: BeautifulSoup = self.get_html_parser(url)
+        if not soup:
+            return
+
+        res = soup.find('h1', id='product-title')
+        if res:
+            return res.text.strip()
--- parser/runner.py
+++ parser/runner.py
@@ -1,3 +1,4 @@
+import re
 from urllib.parse import urlparse
 from lexicon.lexicon import LEXICON_GENERAL_RU
 from models.link_model import LinkModel
@@ -7,6 +8,7 @@ from parser.parser_base import ParserBase
 from parser.amazon import Amazon
 from parser.iherb import Iherb
 from parser.rei import Rei
+from parser.blackdiamondequipment import Blackdiamondequipment
 # }
 
 
@@ -45,10 +47,12 @@ def run() -> list[LinkOrm]:
             # Если цена изменилась добавляем продукт в список
             # и сохраняем изменения в модели
             if price != item.value:
-                item.prev_value = item.value
-                item.value = price
-                result.append(item)
-                LinkModel(item).save()
+                # Сравниваем цены. Если разница меньше 1 то игнорируем изменения
+                if _get_prices_diff(price, item.value):
+                    item.prev_value = item.value
+                    item.value = price
+                    result.append(item)
+                    LinkModel(item).save()
         else:
             raise AttributeError(LEXICON_GENERAL_RU["parser class not found"].format(url=item.url))
     return result
@@ -93,3 +97,16 @@ def _get_class_name(url: str) -> str | None:
     return
 
 
+def _get_prices_diff(price1, price2):
+    def get_num(n):
+        return re.search(r"(\d+.?\d+)", n)
+
+    # Находим цены в строках
+    p1 = get_num(price1)
+    p2 = get_num(price2)
+    if p1 and p2:
+        p1 = float(p1.group())
+        p2 = float(p2.group())
+        res = abs(p1 - p2)  # Получаем абсолютную разницу
+        return True if res > 1 else False  # Если разница меньше 1 то игнорим
+    return True
